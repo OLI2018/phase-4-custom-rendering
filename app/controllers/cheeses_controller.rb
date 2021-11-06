6886083588da9391d0165a138ce4f44be7ba286d
Oleg <roxvemm2014@gmail.com>
2021-11-06 13:14:41 -0700

    initial commit

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,45 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  # end 
+  
   def index
     cheeses = Cheese.all
-    render json: cheeses
-  end
-
+    render json: cheeses, except: [:created_at, :updated_at]
+  end 
+  
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese
+  # end
+
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  # end 
+
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   if cheese
+  #     render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  #   else
+  #     render json: { error: 'Cheese not found' }
+  #   end
+  # end 
+   
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+  end 
+  
 
 end
